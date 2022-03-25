8fbb7b6d0c170300ac5f44232832ec158f10afca
akinaile <aileen-akin@web.de>
2022-03-25 14:54:30 +0100

    Random Poem Update

--- A01_RandomPoem/script.js
+++ A01_RandomPoem/script.js
@@ -1,14 +1,30 @@
 "use strict";
 var randompoem;
 (function (randompoem) {
+    //Arrays: Inhalt der zufälligen Gedichte
     let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibens", "Dementoren"];
-    console.log(subjects);
-    console.log(predicates);
-    console.log(objects);
-    for (let index = 6; index <= subjects.length; index++) {
-        let element = subjects[index];
+    let sentence = "";
+    //nachfrage: obs soweit passt? ja passt
+    //console.log(subjects, predicates, objects);
+    //for Schleife: wenn Wert 1 erreicht ist, wird Gedicht erstellt
+    for (let index = subjects.length; index >= 1; index--) { //index bekommt die Länge von subjects, ist größer/gleich 1, zählt rückwärts
+        if (index == 0) { //der index wird niemals null
+            break;
+        }
+        getVerse(subjects, predicates, objects, sentence); //wenn irgendeine zahl über null ist, soll die Funktion ausgeführt werden
+    }
+    //funktion: Arrays sollen verwendet werden, gedicht soll entstehen
+    function getVerse(_subjects, _predicates, _objects, _sentence) {
+        let randomsubject = Math.floor(Math.random() * subjects.length); //subject erhält eine nummer, die zufällig ist und der länge des subjects entspricht
+        _sentence += _subjects.splice(randomsubject, 1) + " "; //der Satz besteht aus einem zufälligen Wort
+        let randompredicate = Math.floor(Math.random() * predicates.length);
+        _sentence += _predicates.splice(randompredicate, 1) + " ";
+        let randomobject = Math.floor(Math.random() * objects.length);
+        _sentence += _objects.splice(randomobject, 1) + " ";
+        console.log(_sentence);
+        return _sentence;
     }
 })(randompoem || (randompoem = {}));
 //# sourceMappingURL=script.js.map
--- A01_RandomPoem/script.ts
+++ A01_RandomPoem/script.ts
@@ -1,17 +1,37 @@
 namespace randompoem {
 
-
-
+    //Arrays: Inhalt der zufälligen Gedichte
     let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let predicates: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibens", "Dementoren"];
+    let sentence: string = "";
 
-    console.log(subjects);
-    console.log(predicates);
-    console.log(objects);
+    //nachfrage: obs soweit passt? ja passt
+    //console.log(subjects, predicates, objects);
 
-    for (let index = 6; index <= subjects.length; index++) {
-        let element = subjects[index];
-    
+    //for Schleife: wenn Wert 1 erreicht ist, wird Gedicht erstellt
+    for (let index: number = subjects.length; index >= 1; index--) {  //index bekommt die Länge von subjects, ist größer/gleich 1, zählt rückwärts
+        if (index == 0) { //der index wird niemals null
+            break;
+        }
+        getVerse(subjects, predicates, objects, sentence); //wenn irgendeine zahl über null ist, soll die Funktion ausgeführt werden
 }
+
+    //funktion: Arrays sollen verwendet werden, gedicht soll entstehen
+    function getVerse(_subjects: string[], _predicates: string[], _objects: string[], _sentence: string) {
+       
+        let randomsubject: number = Math.floor(Math.random() * subjects.length); //subject erhält eine nummer, die zufällig ist und der länge des subjects entspricht
+        _sentence += _subjects.splice(randomsubject, 1) + " "; //der Satz besteht aus einem zufälligen Wort
+
+        let randompredicate: number = Math.floor(Math.random() * predicates.length);
+        _sentence += _predicates.splice(randompredicate, 1) + " ";
+
+        let randomobject: number = Math.floor(Math.random() * objects.length);
+        _sentence += _objects.splice(randomobject, 1) + " ";
+
+        console.log(_sentence);
+        return _sentence;
+    }
+
+
 }
